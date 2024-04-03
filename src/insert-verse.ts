e1ac5deec1ef7a3c38e3aa85ee3f669a6e3c33a3
Pertempto <pertemptodisce@gmail.com>
2024-04-03 04:58:36 -0400

    Insert code in editor.

--- src/insert-verse.ts
+++ src/insert-verse.ts
@@ -1,16 +1,25 @@
-import * as vscode from "vscode";
+import { window, ExtensionContext } from "vscode";
 
-export async function insertVerse(context: vscode.ExtensionContext) {
-    const result = await vscode.window.showInputBox({
+export async function insertVerse(context: ExtensionContext) {
+    const result = await window.showInputBox({
         value: "John 1:1",
         valueSelection: [0, 10],
         placeHolder: "Verse reference",
         validateInput: (text: string) => {
-            vscode.window.showInformationMessage(`Validating: ${text}`);
             // TODO: check that the text is a valid reference
+            // window.showInformationMessage(`Validating: ${text}`);
             return null;
         },
     });
-    // TODO: insert verse into document
-    vscode.window.showInformationMessage(`Got: ${result}`);
+
+    const editor = window.activeTextEditor;
+    if (editor !== undefined) {
+        // TODO: get the contents of the verse
+        const verseContents = result + '\n';
+        editor.edit((editBuilder) =>
+            editBuilder.insert(editor.selection.active, verseContents)
+        );
+    } else {
+        window.showInformationMessage('No editor open to receive verse!');
+    }
 }
